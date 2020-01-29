ba473081a3cee2817a9a3593d84dbcff5dc1cd2f
rohitsahay <rohitsahay.199626@gmail.com>
2020-01-29 11:41:48 +0530

    login activity/reg activity

--- app/src/main/java/com/example/quoraandroid/LoginActivity.java
+++ app/src/main/java/com/example/quoraandroid/LoginActivity.java
@@ -2,13 +2,59 @@ package com.example.quoraandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView mRegButton;
+    private TextInputEditText mEmail;
+    private TextInputEditText mPassword;
+    private Button mLoginBtn;
+    private Button mGuestBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = (TextInputEditText) findViewById(R.id.login_email);
+        mPassword = (TextInputEditText) findViewById(R.id.login_password);
+        mRegButton = (TextView) findViewById(R.id.reg_txt_btn);
+        mGuestBtn = (Button) findViewById(R.id.login_guest_btn);
+        mLoginBtn = (Button) findViewById(R.id.login_login_btn);
+
+
+       //onClickListeners
+        mRegButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToRegister();
+            }
+        });
+        mGuestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToMain();
+            }
+        });
+
+    }
+
+    private void sendToRegister() {
+        Intent reg_intent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(reg_intent);
+    }
+    private void sendToMain(){
+        Intent main_intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(main_intent);
+        finish();
+
     }
 }
--- app/src/main/java/com/example/quoraandroid/MainActivity.java
+++ app/src/main/java/com/example/quoraandroid/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.quoraandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginBtn = (Button) findViewById(R.id.login_btn);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToLogin();
+            }
+        });
+
+
+    }
+
+    private void sendToLogin() {
+        Intent login_intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(login_intent);
+        finish();
     }
 }
