c0d8174f11dabdee8405d9142025d9a9833b3d51
rohitsahay <rohitsahay.199626@gmail.com>
2020-01-29 13:37:49 +0530

    navbar added-r

--- app/src/main/java/com/example/quoraandroid/MainActivity.java
+++ app/src/main/java/com/example/quoraandroid/MainActivity.java
@@ -1,29 +1,49 @@
 package com.example.quoraandroid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    private Button mLoginBtn;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLoginBtn = (Button) findViewById(R.id.login_btn);
 
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendToLogin();
-            }
-        });
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+
+        );
+          drawerLayout.addDrawerListener(actionBarDrawerToggle);
+          actionBarDrawerToggle.syncState();
+          navigationView.setNavigationItemSelectedListener(this);
+
+
 
 
     }
@@ -32,5 +52,22 @@ public class MainActivity extends AppCompatActivity {
         Intent login_intent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(login_intent);
         finish();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_login:
+                sendToLogin();
+                break;
+            default:
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
